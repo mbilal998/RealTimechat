7082e5a8513722fca35c622cfc16ef121f93e179
MuhammadBilal <m.bilal998@gmail.com>
2021-02-25 17:24:41 +0500

    port file change

--- server.js
+++ server.js
@@ -76,6 +76,6 @@ io.on('connection', socket => {
 
 })
 
-const PORT = 3000 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
